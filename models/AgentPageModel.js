285c16dd7f9724b3f329d9a948553dcccbb8daa7
krisna8745 <krisna8745924951@gmail.com>
2025-06-24 02:40:07 +0530

    Update AgentPageModel.js

--- models/AgentPageModel.js
+++ models/AgentPageModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const agentPageSchema = new mongoose.Schema({
-  username: { type: String, required: true }, // e.g., '8979066955'
-  name: { type: String, required: true },     // e.g., 'Mehraj Ali Khan'
+  username: { type: String, required: true }, 
+  name: { type: String, required: true },    
   password: { type: String, required: true }, // e.g., '123456'
   balance: { type: Number, default: 0 },      // e.g., 10000
   commType: { type: String, default: 'Bet by Bet' },
